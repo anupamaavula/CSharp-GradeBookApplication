a405441a8c8f964dc0a3aae422f73615323a86b9
anupamaavula <anupama_avula@yahoo.com>
2021-04-27 14:40:00 -0400

    Last commit

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,11 +15,11 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
         public bool IsWeighted { get; set; }
-
         public BaseGradeBook(string name,bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
 
@@ -109,20 +109,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa = 4;
+                    break;
+                    
                 case 'B':
-                    return 3;
+                    gpa = 3;
+                    break;
+                    
                 case 'C':
-                    return 2;
+                    gpa = 2;
+                    break;
+                    
                 case 'D':
-                    return 1;
-                case 'F':
-                    return 0;
+                    gpa = 1;
+                    break;
             }
             return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+                gpa++;
+
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
